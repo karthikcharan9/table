44e26c909224f162a5b7892ec90c1b92c1cdca56
karthikcharan9 <38150457+karthikcharan9@users.noreply.github.com>
2020-07-07 20:17:15 +0530

    second commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,6 +5,24 @@ class Home extends Component{
 
     constructor(props) {
         super(props);
+        this.state={
+            Userprofiles:[{isActive: true, _id: "5c4cc2109487b0003924f1e3", 
+            role: "Administrator", 
+            firstName: "Test", 
+            lastName: "Admin"}, 
+{isActive: true, _id: "5e2cc6df7a91aaf820510a55", 
+            role: "Volunteer", 
+            firstName: "Sidd", 
+            lastName: "Test"}, 
+{isActive: true, _id: "5ede6be453a0480017164a5b", 
+            role: "Volunteer", 
+            firstName: "Volunteer 1", 
+            lastName: "Test"},
+{isActive: false, _id: "5ede6be453a0480017164a48", 
+            role: "Volunteer", 
+            firstName: "Volunteer 2", 
+            lastName: "Test"}]
+        }
     }
 
     toLogout=()=>{
@@ -15,7 +33,7 @@ class Home extends Component{
         return(
             <div>
                 <span>Welcome to Home Page!</span><br /><br />
-                <Table />
+                <Table UserData={this.state.Userprofiles}/>
                 <button onClick={this.toLogout}>Logout</button>
             </div>
         )
